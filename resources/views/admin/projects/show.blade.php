dae2b67d95068ef40b9dff5b325954ea19637928
Alethepata <alessia.patane@gmail.com>
2023-11-30 17:59:42 +0100

    [Add] a

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,6 +5,7 @@
     <h1>Progetto : {{$project->title}}</h1>
     <p>Progetto iniziato il : {{$project->start_date }} | Progetto finito il : {{$project->end_date }}</p>
     <p>Descrizione : {{$project->explanation}}</p>
+    <a class="btn btn-outline-warning" href="{{route('admin.projects.edit', $project )}}"><i class="fa-solid fa-pen-to-square"></i></a>
 
 
 
